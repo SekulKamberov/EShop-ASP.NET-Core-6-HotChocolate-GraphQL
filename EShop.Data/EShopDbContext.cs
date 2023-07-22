2732be42c45e9c6535ca153e25bc87f891846585
SK <microfixbg@gmail.com>
2023-07-22 20:50:21 +0300

    Project folder structure
    Project folder structure

---
+++ EShop.Data/EShopDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EShop.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Data
+{
+    public class EShopDbContext : IdentityDbContext<User>
+    {
+        public EShopDbContext(DbContextOptions<EShopDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
